9ad57824d7dc59e4fc24c72dd6267435e3ffaccb
Will Nickson <wfgnickson@gmail.com>
2024-11-12 08:10:54 +0000

    remove old exercise attempts to use Racket instead

--- ex-1.3.scm
+++
@@ -1,6 +0,0 @@
-(define (sumLargerSquares a b c)
-  (let ((sorted (sort (list a b c) >)))
-    (+ (* (car sorted) (car sorted))
-       (* (cadr sorted) (cadr sorted)))))
-
-(display (sumLargerSquares 3 5 7))
