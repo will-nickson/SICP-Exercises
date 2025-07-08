0413b8cd94e43a11777219148d2f2c8537eabf30
Will Nickson <wfgnickson@gmail.com>
2025-07-08 09:03:59 +0100

    complete ex-1.16

---
+++ ex-1.16.rkt
@@ -0,0 +1,11 @@
+#lang sicp
+(define (fast-expt-iter b n a)
+  (cond ((= n 0) a)
+        ((even? n) (fast-expt-iter (* b b) (/ n 2) a))
+        (else (fast-expt-iter b (- n 1) (* a b)))))
+
+(define (fast-expt b n)
+  (fast-expt-iter b n 1))
+
+
+(fast-expt 2 10)
